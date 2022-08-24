36d7c5c23c7fded0e90e0268f0ca748a87bc0646
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-08-24 15:12:55 -0500

    atte

--- src/commands/attend.ts
+++ src/commands/attend.ts
@@ -7,6 +7,12 @@ export const Attend = {
         .setDescription('Attend a meeting')
         .addStringOption((option) => option.setName('email').setDescription('Enter your email')),
     async execute(interaction: CommandInteraction) {
+        const member = interaction.guild?.members.cache
+            .find((member: { id: string; }) => member.id === interaction.user.id);
+        member?.displayName;
+
         
+        const today = new Date();
+
     } 
 }
