2c21cb961dcca617716228a5be6d870865fa7975
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-08-24 14:56:04 -0500

    Finished INdex

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,46 @@
-import { Client, Collection, In } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
+import { commandFiles } from './commands';
 dotenv.config();
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
     ]
 })
+const commands = new Collection();
+
+for (const command of commandFiles) {
+	commands.set(command.data.name, command);
+	console.log(`[+] - Set command ${command.data.name}.`)
+}
+
+/* 
+for (const event of eventFiles) {
+ 	if (event.once) {
+     	client.once(event.name, (...args: any) => event.execute(...args));
+ 	} else {
+		client.on(event.name, (...args: any) => event.execute(...args));
+	}
+ 	console.log(`[+] - Set event ${event.name}.`)
+}
+*/
+
+client.on('interactionCreate', async (interaction: any) => {
+	if (!interaction.isCommand()) return;
+
+	const command: any = commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (e) {
+		console.log(`[x] - ${e}`);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+typeof(process.env.TOKEN) === 'string' ? client.login(process.env.TOKEN) : console.log('[x] - Token not set!');
